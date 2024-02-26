3a24028107d965b5e0fc740a763aa92311816076
gopaladhikari <gopuadks@gmail.com>
2024-02-26 21:03:45 +0545

    updated cors middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,21 +12,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 app.use(
   cors({
     origin: FRONTEND_DOMAIN!,
     credentials: true,
   })
 );
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.get("/", (_, res) => {
   res.json({ message: "Hello from gopal! 🦈" });
