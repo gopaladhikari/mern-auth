8a18a01b3efbdcc7dc9818a44d3197252968f368
gopaladhikari <gopuadks@gmail.com>
2024-02-26 21:05:38 +0545

    removed cookie

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,13 +20,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 app.get("/", (_, res) => {
   res.json({ message: "Hello from gopal! 🦈" });
 });
