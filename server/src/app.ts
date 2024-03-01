61954cb1a003e3d7cf1517bb6d7bbbfa147ef9fb
gopaladhikari <gopuadks@gmail.com>
2024-03-01 15:01:05 +0545

    removed cookie-parser

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,3 @@
-import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 
@@ -11,7 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(cookieParser());
 
 app.use(
   cors({
